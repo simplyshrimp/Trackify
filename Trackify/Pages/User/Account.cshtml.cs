69efc6a1660cb9f4a6af1b71ba70dbaa3dce27d2
cecby0001 <cecby0001@elevcampus.dk>
2025-10-30 15:50:20 +0100

    made account and fixed links

---
+++ Trackify/Pages/User/Account.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Trackify.Domain.Models;
+using Trackify.Service;
+
+namespace Trackify.Pages.User
+{
+    public class AccountModel : PageModel
+    {
+        private readonly IUser userMethod;
+        public AccountModel(IUser user)
+        {
+            userMethod = user;
+        }
+        [BindProperty]
+        public Users user { get; set; }
+        public IActionResult OnGet()
+        {
+            if (HttpContext.Session.GetInt32("LoggedIn") == 1)
+            {
+                user = userMethod.GetUserByID((int)HttpContext.Session.GetInt32("Id"));
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("/Index");
+            }
+        }
+    }
+}
